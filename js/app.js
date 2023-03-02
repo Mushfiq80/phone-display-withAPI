33b62544c8680fb333168f3ba13e09fdf2bf5ba9
Mushfiq80 <aurpon80@gmail.com>
2023-03-02 22:34:02 +0600

    All done & ready

--- js/app.js
+++ js/app.js
@@ -37,6 +37,9 @@ const displayPhones = (phones, dataLimit) => {
                         <div class="card-body">
                             <h5 class="card-title">${phone.phone_name}</h5>
                             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <a onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</a>
+
+            
                         </div>
                     </div>
         `
@@ -58,6 +61,14 @@ document.getElementById('btn-search').addEventListener('click', function(){
     processSearch(10);
 })
 
+// Enter key event on search
+document.getElementById('search-filed').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        processSearch(10);
+    }
+});
+
+
 
 const toggleSpinner = isLoading =>{
     const loaderSection = document.getElementById('loader');
@@ -68,9 +79,29 @@ const toggleSpinner = isLoading =>{
         loaderSection.classList.add('d-none');
     }
 }
-// loadPhone();
+
 
 // not best way to 
 document.getElementById('btn-show-all').addEventListener('click',function(){
     processSearch(); 
 })
+
+const loadPhoneDetails = async id =>{
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhoneDetails(data.data)
+}
+
+const displayPhoneDetails = phone =>{
+    const modalTitle = document.getElementById('phoneDetailModalLabel');
+    modalTitle.innerText = phone.name;
+    const phoneDetails = document.getElementById('phone-detail');
+    phoneDetails.innerHTML = `
+        <p> Release Date: ${phone.releaseDate ? phone.releaseDate : 'No release date found'}</p>
+        <p> Others: ${phone.others ? phone.others.Bluetooth : 'No Bluetooth'}</p>
+        <p> Storage: ${phone.mainFeatures ? phone.mainFeatures.storage : 'No storage info found'}</p>
+    `
+}
+
+loadPhone('apple');
